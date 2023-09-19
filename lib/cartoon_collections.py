52e2c110ea30801b52715a349f511a04345675f5
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-09-19 16:04:57 -0400

    created interation functions

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+CHEESES = ["cheddar", "gouda", "camembert"]
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(names):
+    for i in range(len(names)):
+        print(f"{i + 1}. {names[i]}")
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def summon_captain_planet(planeteer_calls):
+    titled_elements = [e.title() for e in planeteer_calls]
+    return [e + "!" for e in titled_elements]
+
+def long_planeteer_calls(call_list):
+    for c in call_list:
+        if len(c) > 4:
+            return True
+    
+    return False
+
+def find_the_cheese(str_list):
+    for str in str_list:
+        if str in CHEESES:
+            return str
+        
+    return None
